a4ff144e730c7cd072249e9f182a3635b1d954d7
Максим <maksim_goncharov_2006@mail.ru>
2025-05-10 22:21:54 +0300

    Add new loggers for future services. Update wrappers logic: add support for async and sync funks. | Auth Service |

--- auth-service/src/auth_service/log/loggers.py
+++ auth-service/src/auth_service/log/loggers.py
@@ -3,3 +3,9 @@ import logging
 APP_LOGGER: logging.Logger = logging.getLogger("APP")
 EXCEPTION_HANDLER_LOGGER: logging.Logger = logging.getLogger("EXCEPTION HANDLER")
 TEST_LOGGER: logging.Logger = logging.getLogger("TEST")
+
+SESSION_SERVICE_LOGGER: logging.Logger = logging.getLogger("SESSION SERVICE")
+USER_CREDS_SERVICE_LOGGER: logging.Logger = logging.getLogger("USER CREDS SERVICE")
+JWT_SERVICE_LOGGER: logging.Logger = logging.getLogger("JWT SERVICE LOGGER")
+
+USER_CREDS_SERVICE_ROUTER: logging.Logger = logging.getLogger("USER CREDS ROUTER")
