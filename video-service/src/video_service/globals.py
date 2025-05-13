4b4637a5cad4a12d096ba4051be73b8f1e9e3589
Максим <maksim_goncharov_2006@mail.ru>
2025-05-13 12:46:14 +0300

    Change python image version in dockerfile. Add some vars in globals. Rework db_conf. Add redis conf. Rework logging wrappers.

--- video-service/src/video_service/globals.py
+++ video-service/src/video_service/globals.py
@@ -1,9 +1,13 @@
+from dotenv import load_dotenv
 import logging
+import os
+
+load_dotenv()
 
 #
 # SERVER VARS
 #
-PORT = 8082
+PORT = 8085
 HOST = "localhost"
 
 
@@ -13,3 +17,26 @@ HOST = "localhost"
 LOGS_LEVEL = logging.DEBUG
 LOGS_FILENAME = "logs/{date}.log" 
 LOGS_FORMAT = "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+
+
+#
+# DB
+#
+DB_USERNAME = os.environ.get("DB_USERNAME")
+DB_PASSWORD = os.environ.get("DB_PASSWORD")
+DB_NAME = os.environ.get("DB_NAME")
+DB_HOST = os.environ.get("DB_HOST")
+DB_URL = os.environ.get("DB_URL")
+
+
+#
+# REDIS
+#
+REDIS_HOST = os.environ.get("REDIS_HOST")
+REDIS_PORT = int(os.environ.get("REDIS_PORT"))
+
+
+#
+# OTHER
+# 
+JWT_SECRET_KEY = os.environ.get("JWT_SECRET_KEY")
