29d68a585907ce82959ba1fa8a0902be4026f311
Максим <maksim_goncharov_2006@mail.ru>
2025-05-23 17:14:01 +0300

    Add new endpoints for updating/deleting user creds. And add new service logic for updating/deleting user creds. | Auth-Service |

--- auth-service/src/auth_service/routers/user_creds_router.py
+++ auth-service/src/auth_service/routers/user_creds_router.py
@@ -1,45 +1,79 @@
+"""User credentials service module."""
+
+from models.dtos import UserCredsCreate, UserCredsAuth, UserCredsAuthWithToken, UserCredsReturnDto, UserCredsUpdate
+from services.user_creds_service import register, auth, auth_with_jwt_token, delete_user_creds, update_user_creds
 from fastapi import APIRouter, Response
-from models.dtos import UserCredsCreate, UserCredsAuth, UserCredsAuthWithToken
-from services.user_creds_service import register, auth, auth_with_jwt_token
 
 user_creds_router = APIRouter()
 
 @user_creds_router.post("/register")
-async def router_register(resp: Response, user_dto: UserCredsCreate):
-    access_token, refresh_token, session_id = await register(user_dto)
+async def router_register(resp: Response, user_dto: UserCredsCreate) -> UserCredsReturnDto:
+    """Registers a new user.
+    
+    Args:
+        resp (Response): Response object.
+        user_dto (UserCredsCreate): User credentials data transfer object.
+
+    Returns:
+        UserCredsReturnDto: User credentials return data transfer object.
+    """
+
+    user_dto, access_token, refresh_token, session_id = await register(user_dto)
 
     resp.set_cookie("session_id", value=session_id, httponly=True, samesite="strict")
     resp.set_cookie("access_token", value=access_token, httponly=True, samesite="strict")
     resp.set_cookie("refresh_token", value=refresh_token, httponly=True, samesite="strict")
     resp.status_code = 200
 
-    return {"status": "OK"}
+    return user_dto
 
 
 @user_creds_router.post("/auth")
-async def router_auth(user_dto: UserCredsAuth, resp: Response):
-    access_token, refresh_token, session_id = await auth(user_dto)
+async def router_auth(user_dto: UserCredsAuth, resp: Response) -> UserCredsReturnDto:
+    """Authenticates a user.
+    Args:
+        user_dto (UserCredsAuth): User credentials auth data transfer object.
+        resp (Response): Response object.
+    Returns:
+        UserCredsReturnDto: User credentials return data transfer object.
+    """
+
+    user_dto, access_token, refresh_token, session_id = await auth(user_dto)
 
     resp.set_cookie("session_id", value=session_id, httponly=True, samesite="strict")
     resp.set_cookie("access_token", value=access_token, httponly=True, samesite="strict")
     resp.set_cookie("refresh_token", value=refresh_token, httponly=True, samesite="strict")
     resp.status_code = 200
 
-    return {"status": "OK"}
+    return user_dto
 
 
 @user_creds_router.post("/auth_with_token")
-async def router_auth_with_token(token: UserCredsAuthWithToken, resp: Response):
-    _, _, session_id = await auth_with_jwt_token(token.token)
+async def router_auth_with_token(token: UserCredsAuthWithToken, resp: Response) -> UserCredsReturnDto:
+    """Authenticates a user using a JWT token.
+    Args:
+        token (UserCredsAuthWithToken): User credentials auth with token data transfer object.
+        resp (Response): Response object.
+    Returns:
+        UserCredsReturnDto: User credentials return data transfer object.
+    """
+
+    user_dto, _, _, session_id = await auth_with_jwt_token(token.token)
 
     resp.set_cookie("session_id", value=session_id, httponly=True, samesite="strict")
     resp.status_code = 200
 
-    return {"status": "OK"}
+    return user_dto
 
 
 @user_creds_router.post("/refresh")
-async def router_auth_with_token(token: UserCredsAuthWithToken, resp: Response):
+async def refresh_token(token: UserCredsAuthWithToken, resp: Response):
+    """Refreshes the access token using the refresh token.
+    Args:
+        token (UserCredsAuthWithToken): User credentials auth with token data transfer object.
+        resp (Response): Response object.
+    """
+
     access_token, _, session_id = await auth_with_jwt_token(token.token)
 
     resp.set_cookie("session_id", value=session_id, httponly=True, samesite="strict")
@@ -48,3 +82,28 @@ async def router_auth_with_token(token: UserCredsAuthWithToken, resp: Response):
 
     return {"status": "OK"}
 
+
+@user_creds_router.delete("/{user_id}")
+async def delete_user_creds(user_id: str, resp: Response):
+    await delete_user_creds(user_id)
+
+    resp.status_code = 200
+
+    return {"status": "OK", "message": "User deleted successfully"}
+
+
+@user_creds_router.put("/{user_id}")
+async def update_user_creds(user_id: str, user_dto: UserCredsUpdate, resp: Response):
+    """Updates user credentials.
+    
+    Args:
+        user_id (str): User ID.
+        user_dto (UserCredsUpdate): User credentials auth data transfer object.
+        resp (Response): Response object.
+    """
+
+    await update_user_creds(user_id, user_dto)
+
+    resp.status_code = 200
+
+    return {"status": "OK", "message": "User updated successfully"}
--- auth-service/src/auth_service/services/user_creds_service.py
+++ auth-service/src/auth_service/services/user_creds_service.py
@@ -1,28 +1,44 @@
-from globals import JWT_ACCESS_EXPIRATION_TIME_MINUTES, JWT_REFRESH_EXPIRATION_TIME_MINUTES
-from exceptions import ConflictException, BadRequestException, NotFoundException
-from models.dtos import UserCredsCreate, UserCredsAuth
+"""User credentials service module."""
+
+from globals import JWT_ACCESS_EXPIRATION_TIME_MINUTES, JWT_REFRESH_EXPIRATION_TIME_MINUTES, \
+    KAFKA_USER_CREDS_CREATE_TOPIC, KAFKA_USER_CREDS_DELETE_TOPIC
 from log.loggers import USER_CREDS_SERVICE_LOGGER
+
+from exceptions import ConflictException, BadRequestException, NotFoundException, NoContentException
+from models.dtos import UserCredsCreate, UserCredsAuth, UserCredsUpdate, UserCredsReturnDto
+from models.kafka_dtos import UserCredsCreateKafkaDto, UserCredsDeleteKafkaDto
+
 from sqlalchemy.ext.asyncio import AsyncSession
 from config.db_conf import AsyncSessionMaker
 from log.wrappers import log_entrance_debug
-from sqlalchemy import select, or_, and_
 from models.entities import UserCredits
 from sqlalchemy.orm import joinedload
+from sqlalchemy import select, or_
 from passlib.hash import bcrypt
 from typing import List
 
-from .session_service import create_session
 from .jwt_token_service import create_token, validate_token, JwtPayload
+from .session_service import create_session
+from .kafka_service import KafkaProducer
 
 import datetime
 import uuid
 
 
-#
-# Returns access token, refresh token and session_id
-#
 @log_entrance_debug(USER_CREDS_SERVICE_LOGGER)
-async def register(user_dto: UserCredsCreate) -> tuple[str, str, str]:
+async def register(user_dto: UserCredsCreate) -> tuple[UserCredsReturnDto, str, str, str]:
+    """Registers user with username and email and returns access token, refresh token and session_id.
+    
+    Args:
+        user_dto (UserCredsCreate): User credentials data transfer object.
+    
+    Returns:
+        tuple[UserCredsReturnDto, str, str, str]: user credentials data transfer object, access token, refresh token and session_id.
+    
+    Raises:
+        ConflictException: If user with such name or email already exists.
+    """
+    
     async with AsyncSessionMaker() as session:
         session: AsyncSession
 
@@ -55,6 +71,16 @@ async def register(user_dto: UserCredsCreate) -> tuple[str, str, str]:
 
         session.add(new_user)
 
+        await KafkaProducer().send_message(
+            topic=KAFKA_USER_CREDS_CREATE_TOPIC,
+            message=UserCredsCreateKafkaDto(new_user),
+        )
+
+        USER_CREDS_SERVICE_LOGGER.info(f"User was successfilly added | user id {new_user.id}")
+
+        await session.commit()
+        await session.refresh(new_user)
+
         session_id: str = await create_session(
             user_id=new_user.id,
             username=new_user.username,
@@ -73,19 +99,26 @@ async def register(user_dto: UserCredsCreate) -> tuple[str, str, str]:
             exp_time=JWT_REFRESH_EXPIRATION_TIME_MINUTES
         )
 
-        USER_CREDS_SERVICE_LOGGER.info(f"User was successfilly added | user id {new_user.id}")
-
-        await session.commit()
-        await session.refresh(new_user)
-
-        return (access_token, refresh_token, session_id)
+        return (UserCredsReturnDto(new_user), access_token, refresh_token, session_id)
         
 
-#
-# Returns access token, refresh token and session_id
-#
+
 @log_entrance_debug(USER_CREDS_SERVICE_LOGGER)
-async def auth(user_dto: UserCredsAuth) -> tuple[str, str, str]:
+async def auth(user_dto: UserCredsAuth) -> tuple[UserCredsReturnDto, str, str, str]:
+    """Authenticates user with username or email and password and returns access token, refresh token and session_id.
+    
+    Args:
+        user_dto (UserCredsAuth): User credentials data transfer object.
+    
+    Returns:
+        tuple[UserCredsReturnDto, str, str, str]: user credentials data transfer object, access token, refresh token and session_id.
+    
+    Raises:
+        BadRequestException: If user credentials are invalid.
+        NotFoundException: If user with such name or email is not found.
+        ConflictException: If user with such name or email already exists.
+    """
+    
     if user_dto.username == None and user_dto.email == None:
         raise BadRequestException("Invalid creds for authentification")
 
@@ -128,34 +161,150 @@ async def auth(user_dto: UserCredsAuth) -> tuple[str, str, str]:
             exp_time=JWT_REFRESH_EXPIRATION_TIME_MINUTES
         )
 
-        return (access_token, refresh_token, session_id)
+        return (UserCredsReturnDto(user), access_token, refresh_token, session_id)
 
 
-#
-# Returns access token, refresh token and session_id
-#
+
 @log_entrance_debug(USER_CREDS_SERVICE_LOGGER)
-async def auth_with_jwt_token(token: str) -> tuple[str, str, str]:
-    jwt_data: JwtPayload = await validate_token(token)
+async def auth_with_jwt_token(token: str) -> tuple[UserCredsReturnDto, str, str, str]:
+    """Authenticates user with JWT token and returns access token, refresh token and session_id.
+    Args:
+        token (str): JWT token.
+    Returns:
+        tuple[UserCredsReturnDto, str, str, str]: user credentials data transfer object, access token, refresh token and session_id.
+    """
 
-    session_id: str = await create_session(
-        user_id=jwt_data.user_id,
-        username=jwt_data.username,
-        role=jwt_data.role
-    )
-    access_token: str = await create_token(
-        user_id=jwt_data.user_id,
-        username=jwt_data.username,
-        role=jwt_data.role,
-        exp_time=JWT_ACCESS_EXPIRATION_TIME_MINUTES
-    ) 
-    refresh_token: str = await create_token(
-        user_id=jwt_data.user_id,
-        username=jwt_data.username,
-        role=jwt_data.role,
-        exp_time=JWT_REFRESH_EXPIRATION_TIME_MINUTES
-    )
+    async with AsyncSessionMaker() as session:
+        session: AsyncSession
 
-    return (access_token, refresh_token, session_id)
+        jwt_data: JwtPayload = await validate_token(token)
+        if jwt_data == None:
+            raise BadRequestException("Invalid token")
+
+        query = (
+            select(UserCredits)
+            .options(joinedload(UserCredits.role))
+            .where(
+                UserCredits.id == jwt_data.user_id
+            )
+        )
+        user: UserCredits | None = (await session.execute(query)).scalars().first()
+
+        if user == None:
+            raise NotFoundException("Cannot find user with such name or email")
+        
+        if user.username != jwt_data.username or user.role.role != jwt_data.role or user.email != jwt_data.email:
+            raise BadRequestException("Invalid token")
+
+        session_id: str = await create_session(
+            user_id=jwt_data.user_id,
+            username=jwt_data.username,
+            role=jwt_data.role
+        )
+        access_token: str = await create_token(
+            user_id=jwt_data.user_id,
+            username=jwt_data.username,
+            role=jwt_data.role,
+            exp_time=JWT_ACCESS_EXPIRATION_TIME_MINUTES
+        ) 
+        refresh_token: str = await create_token(
+            user_id=jwt_data.user_id,
+            username=jwt_data.username,
+            role=jwt_data.role,
+            exp_time=JWT_REFRESH_EXPIRATION_TIME_MINUTES
+        )
+
+        return (UserCredsReturnDto(user), access_token, refresh_token, session_id)
+
+
+
+@log_entrance_debug(USER_CREDS_SERVICE_LOGGER)
+async def delete_user_creds(user_id: str) -> None:
+    """Deletes user credentials by user id.
+    
+    Args:
+        user_id (str): User id.
+    
+    Raises:
+        NotFoundException: If user with such id is not found.
+    """
+    
+    async with AsyncSessionMaker() as session:
+        session: AsyncSession
+
+        query = (
+            select(UserCredits)
+            .where(
+                UserCredits.id == user_id
+            )
+        )
+        deleting_user: UserCredits | None = (await session.execute(query)).scalars().first()
+
+        if deleting_user == None:
+            raise NotFoundException("Cannot find user with such id")
+
+        await session.delete(deleting_user)
+
+        await KafkaProducer().send_message(
+            topic=KAFKA_USER_CREDS_DELETE_TOPIC,
+            message=UserCredsDeleteKafkaDto(deleting_user),
+        )
+
+        USER_CREDS_SERVICE_LOGGER.info(f"User was successfully deleted | user id {deleting_user.id}")
+
+        await session.commit()
+
+
+
+@log_entrance_debug(USER_CREDS_SERVICE_LOGGER)
+async def update_user_creds(user_id: str, user_dto: UserCredsUpdate) -> UserCredsReturnDto:
+    """Updates user credentials by user id.
+    
+    Args:
+        user_id (str): User id.
+        user_dto (UserCredsUpdate): User credentials data transfer object.
+    
+    Returns:
+        UserCredsReturnDto: User credentials data transfer object.
+
+    Raises:
+        NotFoundException: If user with such id is not found.
+    """
+    
+    async with AsyncSessionMaker() as session:
+        session: AsyncSession
+
+        query = (
+            select(UserCredits)
+            .where(
+                UserCredits.id == user_id
+            )
+        )
+        user: UserCredits | None = (await session.execute(query)).scalars().first()
+
+        if user == None:
+            raise NotFoundException("Cannot find user with such id")
+
+        is_smth_changed: bool = False
+
+        if user_dto.username != None:
+            is_smth_changed = True
+            user.username = user_dto.username
+        if user_dto.email != None:
+            is_smth_changed = True
+            user.email = user_dto.email
+        if user_dto.new_password != None and user_dto.old_password != None:
+            if bcrypt.verify(user_dto.old_password, user.password) == False:
+                raise BadRequestException("Invalid password")
+            else:
+                is_smth_changed = True
+                user.password = bcrypt.hash(user_dto.new_password)
+
+        if is_smth_changed == True:
+            await session.commit()
+            
+            return UserCredsReturnDto(user)
+        else:
+            raise NoContentException("Nothing to update")
 
 
